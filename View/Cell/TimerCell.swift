383121241c3ba3f78701c77e51f3bbaa284fa042
JangJooHyuck <unodos4@infobank.net>
2021-10-31 17:13:36 +0900

    1031_4

--- View/Cell/TimerCell.swift
+++ View/Cell/TimerCell.swift
@@ -11,9 +11,15 @@ class TimerCell:UICollectionViewCell, UITableViewDataSource, UITableViewDelegate
     
     
    
-
+    //테이블뷰
     let myTableView: UITableView = UITableView()
-    let list = ViewModel.VM.MenuList
+    var timerlist : Array = [""]
+    
+    //타이머 시작버튼
+    var startTimerBT: UIButton = UIButton()
+    // 버튼 타이틀
+    
+    
         
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -23,21 +29,24 @@ class TimerCell:UICollectionViewCell, UITableViewDataSource, UITableViewDelegate
         
         self.myTableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         self.addSubview(myTableView)
+        self.addSubview(startTimerBT)
         
         self.myTableView.translatesAutoresizingMaskIntoConstraints = false
+        self.startTimerBT.translatesAutoresizingMaskIntoConstraints = false
         
         TVlayout()
+        TMBTlayout()
         
         
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        list.count
+        timerlist.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as UITableViewCell
         
-        cell.textLabel?.text = list[indexPath.row]
+        cell.textLabel?.text = timerlist[indexPath.row]
         
         return cell
     }
@@ -51,7 +60,41 @@ class TimerCell:UICollectionViewCell, UITableViewDataSource, UITableViewDelegate
         myTableView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10).isActive = true
         myTableView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10).isActive = true
     }
+    func TMBTlayout(){
+        
+        startTimerBT.setTitle("타이머 시작하기", for: .normal)
+        // 버튼 백그라운드 컬러 설정
+        startTimerBT.backgroundColor = UIColor(red: 102/255, green: 100/255, blue: 10/255, alpha: 0.5)
+        // 버튼 원형으로 생성
+        startTimerBT.layer.cornerRadius = 0.2 * startTimerBT.bounds.size.width
+        startTimerBT.clipsToBounds = true
     
+        // 버튼 클릭시 repalaceAction 호출
+        startTimerBT.addTarget(self, action: #selector(addTimer), for: .touchUpInside)
+        
+        
+        startTimerBT.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        startTimerBT.widthAnchor.constraint(equalToConstant: 400).isActive = true
+        startTimerBT.topAnchor.constraint(equalTo: topAnchor, constant: 30).isActive = true
+        startTimerBT.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 0).isActive = true
+        startTimerBT.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20).isActive = true
+        startTimerBT.layer.cornerRadius = 10
+        
+    }
+    
+    @objc func addTimer(sender: UIButton!)
+    {
+        // 버튼 클릭시 애니메이션 설정
+        let colorAnimation = CABasicAnimation(keyPath: "backgroundColor")
+        colorAnimation.fromValue = UIColor.white.cgColor
+        colorAnimation.duration = 1  // animation duration
+        sender.layer.add(colorAnimation, forKey: "ColorPulse")
+        
+        ViewModel.VM.TimerNum += 1
+        self.timerlist.append("[" + String(ViewModel.VM.TimerNum) + "] 현재시간 : " + Date)
+        self.myTableView.reloadData()
+        print("you touched the add timer button")
+    }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
