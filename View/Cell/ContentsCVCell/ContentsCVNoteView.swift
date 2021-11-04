2ac9c99af2bde688a83a153b15b7b77dcaffe4ee
JangJooHyuck <unodos4@infobank.net>
2021-11-05 08:48:52 +0900

    123

--- View/Cell/ContentsCVCell/ContentsCVNoteView.swift
+++ View/Cell/ContentsCVCell/ContentsCVNoteView.swift
@@ -7,16 +7,29 @@
 
 import UIKit
 
-class ContentsCVNoteView:UICollectionViewCell{
+class ContentsCVNoteView:UICollectionViewCell, UITableViewDataSource, UITableViewDelegate{
+    
+    
 
     let NoteText = UILabel()
-    
+    let NoteTableView = UITableView()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         self.addSubview(NoteText)
+        self.addSubview(NoteTableView)
+        
+        NoteTableView.translatesAutoresizingMaskIntoConstraints = false
         NoteText.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.NoteTableView.dataSource = self
+        self.NoteTableView.delegate = self
+        
+        self.NoteTableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        
+        tableviewLayout()
         textLayout()
         
         
@@ -32,6 +45,36 @@ class ContentsCVNoteView:UICollectionViewCell{
         NoteText.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 50).isActive = true
         NoteText.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 50).isActive = true
     }
+    func tableviewLayout() {
+       
+     
+        NoteTableView.heightAnchor.constraint(equalTo: self.heightAnchor).isActive = true
+        NoteTableView.widthAnchor.constraint(equalTo: self.widthAnchor).isActive = true
+        NoteTableView.topAnchor.constraint(equalTo: NoteText.bottomAnchor, constant: 20).isActive = true
+        NoteTableView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10).isActive = true
+        NoteTableView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10).isActive = true
+    }
+    
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as UITableViewCell
+        
+        cell.textLabel?.text = "ㅎㅇ"
+        
+     
+        return cell
+    }
+    
+    
+    
+    
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
