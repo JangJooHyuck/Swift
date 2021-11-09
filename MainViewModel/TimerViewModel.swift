c5450b9ce656208a4c61920c941321caf1259771
JangJooHyuck <unodos4@infobank.net>
2021-11-09 16:19:28 +0900

    시간차구하기성공

--- MainViewModel/TimerViewModel.swift
+++ MainViewModel/TimerViewModel.swift
@@ -17,29 +17,40 @@ class TimerViewModel {
     @Published var isTimerDelete = false
     // 타이머가 돌아간 값이 들어갈 배열
     @Published var timerlist: [Int] = []
-    @Published var timeSet = 0
+    
     @Published var timerlistCount = 0
     
     
    
+    
     func startTimer() {
         
         
         // 시간 포맷
-        
+
         // 버튼을 눌렀을 때 현재의 Date (ex: 2000-01-01 09:14:48 +0000)
         let nowDate = Date()
+
+        let flowDate = nowDate.addingTimeInterval(3600)
         
+       
         // 데이터 포맷
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "a hh시 mm분 ss초"
-        
+
         // PM, AM 을 오전, 오후로 변경
         dateFormatter.locale =  Locale(identifier: "ko_KR")
-        
+
         // 현재 시간의 Date를 format에 맞춰 string으로 반환
         let str = dateFormatter.string(from: nowDate)
+        let str2 = dateFormatter.string(from: flowDate)
+        
+        var leftDate = Int(flowDate.timeIntervalSince(nowDate))
         print(str)
+        print(str2)
+        print(leftDate)
+        
+       
         
         
        
@@ -75,41 +86,7 @@ class TimerViewModel {
         }
        
     }
-            
-            
-//            if TimerViewModel.VM.timerlist.isEmpty == false {
-//
-//                if self.timeSet == 0 {
-//                    print("시간이 입력되지 않았음")
-//                }
-//                else{
-//            // 시간세팅해논거에서 -1 하고.
-//                    self.timeSet -= 1
-//                    print("\(self.timeSet)")
-//            // 현재 시간값을 시분초로 바꿔준 값을 h, m, s 에 저장한다
-//                    let (h, m, s) = self.convertIntToTime(seconds: self.timeSet)
-//
-//            // 배열 값을 변경
-//                    self.timerlist[self.timerlist.count - 1] = String("\(h) 시간 " + "\(m) 분 " + "\(s) 초")
-//            }
-//        }
-//            //타이머가 0이되면
-//            if self.timeSet <= 0 {
-//                //타이머스탑하고
-//                timer.invalidate()
-//                self.timerlist[self.timerlist.count - 1] = ("[시간 종료]" + " 종료시간 : " + "\(str)" )
-//            }
-            
-//            // 타이머 삭제되면 타이머 스탑
-//            if self.isTimerDelete == true {
-//
-//                timer.invalidate()
-//
-//            }
-        
-        
-        //addTimer end
-    
+   
     
     // int 값을 시 분 초로 바꿔준다.
     func convertIntToTime (seconds : Int) -> (Int, Int, Int)
